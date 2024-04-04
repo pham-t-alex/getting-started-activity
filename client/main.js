6ae464b1f8eb1cd9bf71514c212855bbc1c868c4
pham-t-alex <pham.t.alex@gmail.com>
2024-04-04 01:27:38 -0700

    following tutorial

--- client/main.js
+++ client/main.js
@@ -1,5 +1,59 @@
+// Import the SDK
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
 import './style.css'
 import rocketLogo from '/rocket.png'
+
+// Will store authenticated user's access_token
+let auth;
+
+// Instantiate the SDK
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is authenticated");
+})
+
+// Can make API calls within scopes requested in setupDiscordSDK()
+// access_token is secret
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+  console.log("Discord SDK is ready");
+
+  // Authorize with Discord client
+  const { code } = await discordSdk.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: [
+      "identify",
+      "guilds",
+    ],
+  });
+
+  // Retrieve access token
+  const response = await fetch("/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const { access_token } = await response.json();
+
+  // Authenticate with Discord client (using access_token)
+  auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+
+  if (auth == null) {
+    throw new Error("Authenticate command failed");
+  }
+}
 
 document.querySelector('#app').innerHTML = `
   <div>
